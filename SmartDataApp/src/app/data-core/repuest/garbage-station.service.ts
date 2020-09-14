a7992d99a7fea08da922e13097fbf5435bb08ffb
zzl <kila_z@163.com>
2020-09-14 15:53:00 +0800

    地图上传

--- SmartDataApp/src/app/data-core/repuest/garbage-station.service.ts
+++ SmartDataApp/src/app/data-core/repuest/garbage-station.service.ts
@@ -1,154 +1,161 @@
-import { Injectable } from "@angular/core";
-import { SaveModel } from "../model/save-model";
-import { GarbageStation,GetGarbageStationsParams} from "../model/waste-regulation/garbage-station";  
-import { Camera,GetGarbageStationCamerasParams } from "../model/waste-regulation/camera";  
-import {  GetGarbageStationVolumesParams, GarbageVolume } from "../model/waste-regulation/garbage-volume";  
-import { TrashCan,GetGarbageStationTrashCansParams } from "../model/waste-regulation/trashCan";  
-import * as url from "../url/waste-regulation/garbage-station";
-import { PagedList } from "../model/page";
-import { BatchRequest,BatchResult } from "../model/batch";
-import { Response } from "../model/Response"; 
-import { HowellAuthHttpService } from "./howell-auth-http.service";
-import { GetGarbageStationnEventNumbersParams, EventNumberStatistic } from "../model/waste-regulation/division-event-numbers";
-import { GarbageStationNumberStatistic, GetGarbageStationStatisticNumbersParams } from "../model/waste-regulation/garbage-station-number-statistic";
+import { Injectable } from '@angular/core';
+import { SaveModel } from '../model/save-model';
+import { GarbageStation, GetGarbageStationsParams } from '../model/waste-regulation/garbage-station';
+import { Camera, GetGarbageStationCamerasParams } from '../model/waste-regulation/camera';
+import { GetGarbageStationVolumesParams, GarbageVolume } from '../model/waste-regulation/garbage-volume';
+import { TrashCan, GetGarbageStationTrashCansParams } from '../model/waste-regulation/trashCan';
+import * as url from '../url/waste-regulation/garbage-station';
+import { PagedList } from '../model/page';
+import { BatchRequest, BatchResult } from '../model/batch';
+import { Response } from '../model/Response';
+import { HowellAuthHttpService } from './howell-auth-http.service';
+import { GetGarbageStationnEventNumbersParams, EventNumberStatistic } from '../model/waste-regulation/division-event-numbers';
+import {
+    GarbageStationNumberStatistic, GetGarbageStationStatisticNumbersParams
+} from '../model/waste-regulation/garbage-station-number-statistic';
 @Injectable({
-    providedIn:'root'
+    providedIn: 'root'
 })
-export class GarbageStationRequestService extends SaveModel{
+export class GarbageStationRequestService extends SaveModel {
     url: url.GarbageStations;
     constructor(private requestService: HowellAuthHttpService) {
         super();
         this.url = new url.GarbageStations();
     }
-    create(item:GarbageStation){ 
-        return this.requestService.post<GarbageStation, Response<GarbageStation>>(this.url.create(), this.toModel(item,this.formMustField.garbageStation));
-    } 
+    create(item: GarbageStation) {
+        return this.requestService.post<GarbageStation,
+            Response<GarbageStation>>(this.url.create(), this.toModel(item, this.formMustField.garbageStation));
+    }
 
     get(id: string) {
         return this.requestService.get<GarbageStation>(this.url.get(id));
     }
 
-    set(item: GarbageStation){
-        return this.requestService.put<GarbageStation, Response<GarbageStation>>(this.url.edit(item.Id), this.toModel(item,this.formMustField.garbageStation));
+    set(item: GarbageStation) {
+        return this.requestService.put<GarbageStation,
+            Response<GarbageStation>>(this.url.edit(item.Id), this.toModel(item, this.formMustField.garbageStation));
     }
 
     del(id: string) {
         return this.requestService.delete<GarbageStation>(this.url.del(id));
     }
 
-    list(item:GetGarbageStationsParams){
+    list(item: GetGarbageStationsParams) {
         return this.requestService.post<GetGarbageStationsParams, Response<PagedList<GarbageStation>>>(this.url.list(), item);
-    } 
-
-    volumesHistory(item:GetGarbageStationVolumesParams,divisionsId:string){
-        return this.requestService.post<GetGarbageStationVolumesParams, Response<PagedList<GarbageVolume>>>(this.url.volumesHistory(divisionsId), item);
     }
 
-    eventNumbersHistory(item:GetGarbageStationnEventNumbersParams,divisionsId:string){
-        return this.requestService.post<GetGarbageStationnEventNumbersParams, Response<PagedList<EventNumberStatistic>>>(this.url.eventNumbersHistory(divisionsId), item);
+    volumesHistory(item: GetGarbageStationVolumesParams, divisionsId: string) {
+        return this.requestService.post<GetGarbageStationVolumesParams,
+            Response<PagedList<GarbageVolume>>>(this.url.volumesHistory(divisionsId), item);
+    }
+
+    eventNumbersHistory(item: GetGarbageStationnEventNumbersParams, divisionsId: string) {
+        return this.requestService.post<GetGarbageStationnEventNumbersParams,
+            Response<PagedList<EventNumberStatistic>>>(this.url.eventNumbersHistory(divisionsId), item);
     }
 
     statisticNumber(divisionsId: string) {
         return this.requestService.get<GarbageStationNumberStatistic>(this.url.statisticNumber(divisionsId));
     }
 
-    statisticNumberList(item:GetGarbageStationStatisticNumbersParams){
-        return this.requestService.post<GetGarbageStationStatisticNumbersParams, Response<PagedList<GarbageStationNumberStatistic>>>(this.url.statisticNumberList(), item);
+    statisticNumberList(item: GetGarbageStationStatisticNumbersParams) {
+        return this.requestService.post<GetGarbageStationStatisticNumbersParams,
+            Response<PagedList<GarbageStationNumberStatistic>>>(this.url.statisticNumberList(), item);
     }
 }
 
 @Injectable({
-    providedIn:'root'
+    providedIn: 'root'
 })
-export class CameraRequestService extends SaveModel{
+export class CameraRequestService extends SaveModel {
     url: url.Camera;
     constructor(private requestService: HowellAuthHttpService) {
         super();
         this.url = new url.Camera();
     }
-    create(item:Camera){ 
-        return this.requestService.post<Camera, Response<Camera>>(this.url.create(item.GarbageStationId),item);
-    } 
-
-    get(garbageStationId:string,cameraId: string) {
-        return this.requestService.get<Camera>(this.url.get(garbageStationId,cameraId));
+    create(item: Camera) {
+        return this.requestService.post<Camera, Response<Camera>>(this.url.create(item.GarbageStationId), item);
     }
 
-    list(garbageStationId:string){
-        return this.requestService.get<Response<Camera[]>>(this.url.create(garbageStationId));
-    }     
-
-    set(item: Camera){
-        return this.requestService.put<Camera, Response<Camera>>(this.url.edit(item.GarbageStationId,item.Id), item);
+    get(garbageStationId: string, cameraId: string) {
+        return this.requestService.get<Camera>(this.url.get(garbageStationId, cameraId));
     }
 
-    del(garbageStationId:string,cameraId: string) {
-        return this.requestService.delete<Camera>(this.url.del(garbageStationId,cameraId));
+    list(garbageStationId: string) {
+        return this.requestService.get<Camera[]>(this.url.create(garbageStationId));
     }
 
-    postList(item:GetGarbageStationsParams){
+    set(item: Camera) {
+        return this.requestService.put<Camera, Response<Camera>>(this.url.edit(item.GarbageStationId, item.Id), item);
+    }
+
+    del(garbageStationId: string, cameraId: string) {
+        return this.requestService.delete<Camera>(this.url.del(garbageStationId, cameraId));
+    }
+
+    postList(item: GetGarbageStationsParams) {
         return this.requestService.post<GetGarbageStationCamerasParams, Response<PagedList<Camera>>>(this.url.list(), item);
-    } 
+    }
 }
 
 
 @Injectable({
-    providedIn:'root'
+    providedIn: 'root'
 })
-export class CameraTrashCanRequestService extends SaveModel{
+export class CameraTrashCanRequestService extends SaveModel {
     url: url.CameraTrashCans;
     constructor(private requestService: HowellAuthHttpService) {
         super();
         this.url = new url.CameraTrashCans();
     }
-    create(item:TrashCan){ 
-        return this.requestService.post<TrashCan, Response<TrashCan>>(this.url.create(item.GarbageStationId,item.CameraId),item);
-    } 
-
-    get(garbageStationId:string,cameraId: string,trashCanId:string) {
-        return this.requestService.get<TrashCan>(this.url.get(garbageStationId,cameraId,trashCanId));
+    create(item: TrashCan) {
+        return this.requestService.post<TrashCan, Response<TrashCan>>(this.url.create(item.GarbageStationId, item.CameraId), item);
     }
 
-    set(item: TrashCan){
-        return this.requestService.put<TrashCan, Response<TrashCan>>(this.url.edit(item.GarbageStationId,item.CameraId,item.Id), item);
+    get(garbageStationId: string, cameraId: string, trashCanId: string) {
+        return this.requestService.get<TrashCan>(this.url.get(garbageStationId, cameraId, trashCanId));
     }
 
-    del(garbageStationId:string,cameraId: string,trashCanId:string) {
-        return this.requestService.delete<TrashCan>(this.url.del(garbageStationId,cameraId,trashCanId));
+    set(item: TrashCan) {
+        return this.requestService.put<TrashCan, Response<TrashCan>>(this.url.edit(item.GarbageStationId, item.CameraId, item.Id), item);
     }
 
-    list(garbageStationId:string,cameraId: string){
-        return this.requestService.get<Response<TrashCan[]>>(this.url.list(garbageStationId,cameraId));
-    } 
+    del(garbageStationId: string, cameraId: string, trashCanId: string) {
+        return this.requestService.delete<TrashCan>(this.url.del(garbageStationId, cameraId, trashCanId));
+    }
+
+    list(garbageStationId: string, cameraId: string) {
+        return this.requestService.get<Response<TrashCan[]>>(this.url.list(garbageStationId, cameraId));
+    }
 }
 
 
 @Injectable({
-    providedIn:'root'
+    providedIn: 'root'
 })
-export class GarbageStationTrashCanRequestService extends SaveModel{
+export class GarbageStationTrashCanRequestService extends SaveModel {
     url: url.GarbageStationTrashCans;
     constructor(private requestService: HowellAuthHttpService) {
         super();
         this.url = new url.GarbageStationTrashCans();
     }
-    create(item:TrashCan){ 
-        return this.requestService.post<TrashCan, Response<TrashCan>>(this.url.create(item.GarbageStationId),item);
-    } 
-
-    get(garbageStationId:string,cameraId: string) {
-        return this.requestService.get<TrashCan>(this.url.get(garbageStationId,cameraId));
+    create(item: TrashCan) {
+        return this.requestService.post<TrashCan, Response<TrashCan>>(this.url.create(item.GarbageStationId), item);
     }
 
-    set(item: TrashCan){
-        return this.requestService.put<TrashCan, Response<TrashCan>>(this.url.edit(item.GarbageStationId,item.Id), item);
+    get(garbageStationId: string, cameraId: string) {
+        return this.requestService.get<TrashCan>(this.url.get(garbageStationId, cameraId));
     }
 
-    del(garbageStationId:string,cameraId: string) {
-        return this.requestService.delete<TrashCan>(this.url.del(garbageStationId,cameraId));
+    set(item: TrashCan) {
+        return this.requestService.put<TrashCan, Response<TrashCan>>(this.url.edit(item.GarbageStationId, item.Id), item);
     }
 
-    list(item:GetGarbageStationTrashCansParams){
+    del(garbageStationId: string, cameraId: string) {
+        return this.requestService.delete<TrashCan>(this.url.del(garbageStationId, cameraId));
+    }
+
+    list(item: GetGarbageStationTrashCansParams) {
         return this.requestService.post<GetGarbageStationTrashCansParams, Response<PagedList<TrashCan>>>(this.url.postList(), item);
-    } 
+    }
 }
---
+++ SmartDataApp/src/app/waste-regulation-system/index/amap/amap.component.ts
@@ -0,0 +1,96 @@
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Camera } from 'src/app/data-core/model/waste-regulation/camera';
+import { GarbageStation, GetGarbageStationsParams } from 'src/app/data-core/model/waste-regulation/garbage-station';
+import { CameraRequestService, GarbageStationRequestService } from 'src/app/data-core/repuest/garbage-station.service';
+
+import { AMapService } from './amap.service';
+
+
+@Component({
+    selector: 'app-amap',
+    templateUrl: './amap.component.html',
+    styleUrls: ['./amap.component.css'],
+    providers: [AMapService]
+})
+export class AMapComponent implements AfterViewInit, OnInit {
+
+    selectedCameras: Camera[] = [];
+    garbages: GarbageStation[];
+    // amapSrc: any;
+    srcUrl: any;
+    dataController: CesiumDataController.Controller; // CesiumDataController.Controller;
+    client: CesiumMapClient;
+
+    constructor(
+        private sanitizer: DomSanitizer,
+        private garbageService: GarbageStationRequestService,
+        private cameraService: CameraRequestService
+    ) {
+        this.srcUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.getSrc());
+
+    }
+
+    getSrc() {
+        const host = document.location.hostname;
+        const port = document.location.port;
+        return 'http://' + host + ':' + port + '/amap/map_ts.html?maptype=AMapOffline&v=20191106';
+    }
+
+    ngOnInit() {
+        // const promise = this.garbageService.list(new GetGarbageStationsParams()).toPromise();
+        // promise.then((response) => {
+        //     this.garbages = response.Data.Data;
+        // });
+    }
+    OnCameraClicked(camera: Camera) {
+
+    }
+    ngAfterViewInit() {
+        // Detect effects of NgForTrackBy
+        this.client = new CesiumMapClient('iframe');
+        this.client.Events.OnLoading = () => {
+            // for (var i = 0; i < 100; i++) {
+            //     AddAlarm(i * 10);
+            // }
+
+            console.log('client.Events.OnLoading');
+            this.dataController = this.client.DataController;
+        };
+        this.client.Events.OnLoaded = () => {
+
+
+
+
+            const villages = this.dataController.Village.Point.List();
+            for (const villageId in villages) {
+                if (Object.prototype.hasOwnProperty.call(villages, villageId)) {
+                    const village = villages[villageId];
+                    // village.
+                }
+            }
+        };
+
+        this.client.Events.OnElementsDoubleClicked = (objs) => {
+            if (!objs || objs.length <= 0) { return; }
+            const id = objs[0].id;
+            const list = document.getElementsByClassName('map-bar video-list')[0];
+            if (list && objs && objs.length > 0) {
+                list['style'].display = 'block';
+                const promise = this.cameraService.list(id).toPromise();
+                promise.then((response) => {
+                    this.selectedCameras = response.Data;
+                });
+            }
+
+        };
+        this.client.Events.OnElementsClicked = function (objs) {
+            if (!objs || objs.length === 0) {
+                const list = document.getElementsByClassName('map-bar video-list')[0];
+                list['style'].display = 'none';
+            }
+        };
+    }
+
+}
+
