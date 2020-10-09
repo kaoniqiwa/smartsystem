5f2260540e40219ca5229d3b59e59e0062147594
zzl <kila_z@163.com>
2020-10-09 13:33:09 +0800

    image error

--- SmartDataApp/src/app/waste-regulation-system/index/amap/amap.component.ts
+++ SmartDataApp/src/app/waste-regulation-system/index/amap/amap.component.ts
@@ -149,7 +149,7 @@ export class AMapComponent implements AfterViewInit, OnInit {
                             const camera_response = await this.aiopCameraService.get(datas[i].Id).toPromise();
                             if (camera_response) {
                                 if (camera_response.Data.ImageUrl) {
-                                    camera_response.Data.ImageUrl = this.mediaService.getData(camera_response.Data.ImageUrl);
+                                    camera_response.Data.ImageUrl = this.mediaService.getData(camera_response.Data.ImageUrl) + 'ssss';
                                 } else {
                                     camera_response.Data.ImageUrl = 'assets/img/timg.png';
                                 }
@@ -207,6 +207,10 @@ export class AMapComponent implements AfterViewInit, OnInit {
     }
     ngAfterViewInit() {
 
+    }
+
+    OnImageError(evt: Event) {
+        (evt.currentTarget as HTMLImageElement).src = 'assets/img/timg.png';
     }
 
     OnVillageWindowClosed() {
