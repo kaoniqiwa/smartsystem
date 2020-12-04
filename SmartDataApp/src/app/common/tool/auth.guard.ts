ddd270ee369b498feebe27066c1e886942fb6b51
swb <495146161@qq.com>
2020-12-04 09:38:27 +0800

    复制网址 限制

--- SmartDataApp/src/app/common/tool/auth.guard.ts
+++ SmartDataApp/src/app/common/tool/auth.guard.ts
@@ -1,24 +1,24 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, UrlTree } from '@angular/router';
+import { log } from 'console';
 import { SessionUser } from "../tool/session-user";
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  constructor(private router: Router) {}
+  constructor(private router: Router) { }
 
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): true|UrlTree {
-    let url: string = state.url;
-
+    state: RouterStateSnapshot): true | UrlTree {
+    let url: string = state.url; 
     return this.checkLogin(url);
   }
 
-  checkLogin(url: string): true|UrlTree {
-    const user = new SessionUser();
-    if (user.name&&user.pwd) { return true; } 
+  checkLogin(url: string): true | UrlTree {
+    const user = new SessionUser(), digestAuth = sessionStorage.getItem('WWW-Authenticate');
+    if (user.name && user.pwd&&digestAuth) { return true; }
 
     // Redirect to the login page
     return this.router.parseUrl('/login');
