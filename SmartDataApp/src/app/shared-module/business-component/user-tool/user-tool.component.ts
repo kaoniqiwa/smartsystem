d923720f4fa2d5f559330b2a8cda1ae159519d8a
swb <495146161@qq.com>
2020-12-31 14:57:29 +0800

    登出

--- SmartDataApp/src/app/shared-module/business-component/user-tool/user-tool.component.ts
+++ SmartDataApp/src/app/shared-module/business-component/user-tool/user-tool.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { SessionUser } from "../../../common/tool/session-user";
-import { Router } from '@angular/router';
-import { StatisticalDataBufferService } from '../../../waste-regulation-system/index/business-card-grid/buffer/statistical-data-buffer';
-import { MQTTEventService } from '../../../common/tool/mqtt-event/mqtt-event.service';
 @Component({
   selector: 'app-user-tool',
   templateUrl: './user-tool.component.html'
@@ -10,21 +7,15 @@ import { MQTTEventService } from '../../../common/tool/mqtt-event/mqtt-event.ser
 export class UserToolComponent implements OnInit {
   sessionUser = new SessionUser();
 
-  constructor(private router: Router,private buffer:StatisticalDataBufferService
-    ,private mqttService:MQTTEventService) {
-
-
+  constructor() {
   }
 
   ngOnInit() {
-
   }
 
   logOut() {
     this.sessionUser.clear = true;
-    this.router.navigateByUrl('login');
-    this.buffer.cacheReset(); 
-    this.mqttService.unlistenerIllegalDrop();
+  window.location.href = '/login'
   }
 
 }
