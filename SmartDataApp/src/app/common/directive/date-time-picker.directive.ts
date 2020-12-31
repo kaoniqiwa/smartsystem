bec65a52a7933648986e41e3741edac661592a9a
swb <495146161@qq.com>
2020-12-31 14:52:19 +0800

    销毁订阅event

--- SmartDataApp/src/app/common/directive/date-time-picker.directive.ts
+++ SmartDataApp/src/app/common/directive/date-time-picker.directive.ts
@@ -9,12 +9,11 @@ declare var $: any;
 })
 export class DateTimePickerDirective implements AfterContentInit {
   private ele: any;
-
   @Input('format') format = 'yyyy-mm-dd';
   @Input('defaultVal') defaultVal = '';
   @Input('changeDate') changeDate: (val: any) => void;
   @Input('startView') startView = 2;
-  @Input('minView') minView = 2;
+  @Input('minView') minView = 2; 
   constructor(e: ElementRef, private datePipe: DatePipe) {
     this.ele = e.nativeElement;
   }
@@ -31,14 +30,18 @@ export class DateTimePickerDirective implements AfterContentInit {
       initialDate: this.defaultVal ? this.defaultVal.dateTimePickerZC() : new Date(),
     }).on('changeDate', (ev) => {
       if (this.changeDate) this.changeDate(ev.date);
+    }).on('show', (ev) => {
+      const dayDom = $('.datetimepicker-days');
+      dayDom.find('.week-tr').removeClass('week-tr');
     });
     $(this.ele).val(this.defaultVal);
   }
 
   reInit(startView: number, minView: number, format: string, defaultVal: string, week?: boolean) {
-    $(this.ele).val('');
-    $(this.ele).datetimepicker('remove');
-    if (week){
+   
+    $(this.ele).val(''); 
+    $(this.ele).datetimepicker('remove').off('changeDate').off('show');
+    if (week) {  
       $(this.ele).datetimepicker({
         format: format,
         weekStart: 1,
@@ -47,33 +50,36 @@ export class DateTimePickerDirective implements AfterContentInit {
         minView: minView,
         language: 'zh-CN',
         forceParse: false,
-      }).on('changeDate', (ev) => {
-        if (this.changeDate) this.changeDate(ev.date);
-        const week = OneWeekDate(ev.date); 
-        $(this.ele).val(`${this.datePipe.transform(week.monday,'yyyy年MM月dd日')} 至 ${this.datePipe.transform(week.sunday,'yyyy年MM月dd日')}`);
+        initialDate: defaultVal ? defaultVal.dateTimePickerZC() : new Date(),
+      }).on('changeDate', (ev) => { 
+      
+          if (this.changeDate) this.changeDate(ev.date);
+          const week_ = OneWeekDate(ev.date);
+          $(this.ele).val(`${this.datePipe.transform(week_.monday, 'yyyy年MM月dd日')} 至 ${this.datePipe.transform(week_.sunday, 'yyyy年MM月dd日')}`);
      
       }).on('show', (ev) => {
-        const dayDom = $('.datetimepicker-days');
-        dayDom.addClass('week');
-        var tbody = dayDom.find('tbody'),
-          trs = tbody.find('tr'),
-          d = this.datePipe.transform(ev.date,'dd');
-        d = parseInt(d) + '';
-        $(trs).each(function (index:number, element:any) {
-          var tds = $(element).children();
-        
-          $(tds).each(function (i:number, el:any) {
-            if ($(el).hasClass('old') == false && $(el).hasClass('new') == false && $(el).text() == d) {
-              $(el).parent().addClass('week-tr');
-            }
+      
+          const dayDom = $('.datetimepicker-days');
+          dayDom.find('.week-tr').removeClass('week-tr');
+          dayDom.addClass('week');
+          var tbody = dayDom.find('tbody'),
+            trs = tbody.find('tr'),
+            d = this.datePipe.transform(ev.date, 'dd');
+          d = parseInt(d) + '';console.log(d);
+          
+          $(trs).each(function (index: number, element: any) {
+            var tds = $(element).children();
+            $(tds).each(function (i: number, el: any) {
+              if ($(el).hasClass('old') == false && $(el).hasClass('new') == false && $(el).text() == d) {
+                $(el).parent().addClass('week-tr');
+              }
+            });
           });
-
-        })
-      });
-      const week = OneWeekDate(new Date(defaultVal.dateTimePickerZC())); 
-      $(this.ele).val(`${this.datePipe.transform(week.monday,'yyyy年MM月dd日')} 至 ${this.datePipe.transform(week.sunday,'yyyy年MM月dd日')}`);
+      });   
+      const week_ = OneWeekDate(new Date(defaultVal.dateTimePickerZC())); 
+      $(this.ele).val(`${this.datePipe.transform(week_.monday, 'yyyy年MM月dd日')} 至 ${this.datePipe.transform(week_.sunday, 'yyyy年MM月dd日')}`);
     }
-    else{
+    else {
       $(this.ele).datetimepicker({
         format: format,
         weekStart: 1,
@@ -82,8 +88,12 @@ export class DateTimePickerDirective implements AfterContentInit {
         minView: minView,
         language: 'zh-CN',
         forceParse: false,
+        initialDate: defaultVal ? defaultVal.dateTimePickerZC() : new Date(),
       }).on('changeDate', (ev) => {
         if (this.changeDate) this.changeDate(ev.date);
+      }).on('show', (ev) => {
+        const dayDom = $('.datetimepicker-days');
+        dayDom.find('.week-tr').removeClass('week-tr');
       });
       $(this.ele).val(defaultVal);
     }
