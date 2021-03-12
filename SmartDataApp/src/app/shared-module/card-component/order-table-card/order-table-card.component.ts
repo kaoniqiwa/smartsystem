cabdc14a0a892cd283bbcc62f6c467ec3db25006
swb <495146161@qq.com>
2021-03-12 16:51:57 +0800

    右侧加droplist

--- SmartDataApp/src/app/shared-module/card-component/order-table-card/order-table-card.component.ts
+++ SmartDataApp/src/app/shared-module/card-component/order-table-card/order-table-card.component.ts
@@ -1,18 +1,22 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { BasisCardComponent, ViewsModel } from '../../../common/abstract/base-view';
-import {OrderTable  } from "./order-table";
+import { OrderTable } from "./order-table";
 @Component({
   selector: 'app-order-table-card',
   templateUrl: './order-table-card.component.html'
 })
-export class OrderTableCardComponent extends BasisCardComponent  implements OnInit {
-@Input() model:OrderTable;
-constructor() {
-  super();
- }
+export class OrderTableCardComponent extends BasisCardComponent implements OnInit {
+  @Input() model: OrderTable;
 
-ngOnInit() {
-  this.loadDatas(new ViewsModel());
-}
+  selectedItemFn = (id: string) => {
+    if (this.btnControl) this.btnControl({ id:id,eventType:this.model.dropList.eventType});
+  }
+  constructor() {
+    super();
+  }
+
+  ngOnInit() {
+    this.loadDatas(new ViewsModel());
+  }
 
 }
--- SmartDataApp/src/app/shared-module/card-component/order-table-card/order-table.ts
+++ SmartDataApp/src/app/shared-module/card-component/order-table-card/order-table.ts
@@ -4,5 +4,14 @@ export class OrderTable{
         name:string;
         subName:string;
         subNameAfter:string;
-    }[]
+    }[];
+    dropList:{
+        listNodes:Array<{
+            id:string;
+            name:string; 
+         }>;
+         fontSize:string;
+         defaultId:string;
+         eventType:number;
+    }
 }
