90cdc1be2dba0cf80e1f95f6f66157d23902725f
swb <495146161@qq.com>
2021-03-29 10:48:03 +0800

    游览器页面缓存

--- SmartDataApp/src/app/app.component.ts
+++ SmartDataApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core'; 
+import { Meta } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -6,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   // title = 'SmartDataApp'; 
+  constructor(  private meta: Meta){
+    
+    this.meta.addTag({ httpEquiv: 'cache-control', content: 'no-cache,no-store, must-revalidate' });
+    this.meta.addTag({ httpEquiv: 'pragma', content: 'no-cache' });
+    this.meta.addTag({ httpEquiv: 'Expires', content: '0' });
+  }
 }
