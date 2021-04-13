0da60e7f063796cec4198de79f7306146e46bb55
swb <495146161@qq.com>
2021-04-13 17:28:15 +0800

    枚举

---
+++ SmartDataApp/src/app/shared-module/business-component/camera-img-url.ts
@@ -0,0 +1,26 @@
+
+import { CameraImageUrl } from "../../data-core/model/waste-regulation/event-record"; 
+
+export class HWCameraImageUrl extends CameraImageUrl{
+    id:string; 
+    imgType:ImgTypeEnum;
+    typeName:TypeNameEnum;
+}
+
+export enum ImgTypeEnum{
+    drop='drop',
+    handle='handle',
+    timeOut='timeOut'
+}
+
+export enum TypeNameEnum{
+    drop='垃圾落地',
+    handle='已处置',
+    timeOut='超时未处置'
+}
+
+export enum TypeNameColorEnum{
+    drop='sky-blue-text2',
+    handle='orange-text',
+    timeOut='powder-red-text'
+}
