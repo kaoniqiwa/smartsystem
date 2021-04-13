bc0ec38fd97488b7e6e6af7cf021460e4ec54ac9
swb <495146161@qq.com>
2021-04-13 17:25:51 +0800

    导报

--- SmartDataApp/src/app/app.component.ts
+++ SmartDataApp/src/app/app.component.ts
@@ -9,8 +9,12 @@ export class AppComponent {
   // title = 'SmartDataApp'; 
   constructor(  private meta: Meta){
     
-    this.meta.addTag({ httpEquiv: 'cache-control', content: 'no-cache,no-store, must-revalidate' });
+    this.meta.addTag({ httpEquiv: 'cache-control', content: 'no-cache,no-store,max-age=0, must-revalidate' });
     this.meta.addTag({ httpEquiv: 'pragma', content: 'no-cache' });
-    this.meta.addTag({ httpEquiv: 'Expires', content: '0' });
+    this.meta.addTag({ httpEquiv: 'Expires', content: 'Tue, 13 Apr 2021 11:35:00  GMT' });
+  //   console.log(new Date('2021-4-13 11:33').toString());//Tue, 13 Apr 2021 11:33:00  GMT  Fri, 12 Jan 2001 18:18:18 GMT
+     //Tue Apr 13 2021 11:33:00 
+ 
+    
   }
 }
--- SmartDataApp/src/app/shared-module/card-component/hint-card/hint.ts
+++ SmartDataApp/src/app/shared-module/card-component/hint-card/hint.ts
@@ -14,5 +14,6 @@ export enum HintTag{
     IllegalDrop=1,
     MixedInto,
     FullStation,
-    GarbageStation 
+    GarbageStation,
+    StationStranded
 }
