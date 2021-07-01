96057574ad0b9755f0b6b29527e58b96998b4ef8
tuzhongyi <tuzhongyi@howell.net.cn>
2021-07-01 16:56:00 +0800

    debug 按回车键会重新登录

--- SmartDataApp/src/app/user-authentication/user-login/user-login.component.ts
+++ SmartDataApp/src/app/user-authentication/user-login/user-login.component.ts
@@ -1,43 +1,50 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { enterKeyDown, createVideo } from "../../common/tool/jquery-help/jquery-help";
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import {
+  enterKeyDown,
+  createVideo,
+} from "../../common/tool/jquery-help/jquery-help";
 import { UserLoginService } from "./user-login.service";
-import { Title } from '@angular/platform-browser';
-import { ActivatedRoute } from '@angular/router'; 
+import { Title } from "@angular/platform-browser";
+import { ActivatedRoute } from "@angular/router";
 @Component({
-  selector: 'app-user-login',
-  templateUrl: './user-login.component.html',
-  styleUrls: ['./user-login.component.styl'],
-  providers: [UserLoginService]
+  selector: "app-user-login",
+  templateUrl: "./user-login.component.html",
+  styleUrls: ["./user-login.component.styl"],
+  providers: [UserLoginService],
 })
 export class UserLoginComponent implements OnInit {
-  @ViewChild('userName')
+  @ViewChild("userName")
   userName: ElementRef;
 
   showBg = true;
 
-  constructor(private userLoginService: UserLoginService, titleService: Title
-    , private route: ActivatedRoute) {
-    titleService.setTitle('用户登录');
+  constructor(
+    private userLoginService: UserLoginService,
+    titleService: Title,
+    private route: ActivatedRoute
+  ) {
+    titleService.setTitle("用户登录");
   }
 
   ngOnInit() {
     this.userName.nativeElement.focus();
     enterKeyDown(() => {
-      this.login();
+      this.login().then((x) => {
+        enterKeyDown(undefined);
+      });
     });
     this.userLoginService.fillUserForm();
-    createVideo('videoLogin', 'assets/img/login.webm', 'videoWrap');
+    createVideo("videoLogin", "assets/img/login.webm", "videoWrap");
     setTimeout(() => {
       this.showBg = false;
     }, 1000);
-    this.route.queryParams.subscribe(params => {
+    this.route.queryParams.subscribe((params) => {
       setTimeout(() => {
         /**url 授权登录 */
         this.userLoginService.urlAuthLogin({
-          Auto:params.Auth
+          Auto: params.Auth,
         });
       }, 1200);
-        
     });
   }
 
@@ -48,10 +55,12 @@ export class UserLoginComponent implements OnInit {
 
   pwdCheck() {
     this.userLoginService.jpwd_ = !this.userLoginService.jpwd_;
-    if (this.userLoginService.jpwd_ === false) { this.userLoginService.autoLogin_ = false; }
+    if (this.userLoginService.jpwd_ === false) {
+      this.userLoginService.autoLogin_ = false;
+    }
   }
 
   login() {
-    this.userLoginService.login();
+    return this.userLoginService.login();
   }
 }
--- SmartDataApp/src/app/user-authentication/user-login/user-login.service.ts
+++ SmartDataApp/src/app/user-authentication/user-login/user-login.service.ts
@@ -9,138 +9,164 @@ import { HowellAuthHttpService } from "../../data-core/repuest/howell-auth-http.
 import { User } from "../../data-core/url/user-url";
 import { BaseUrl } from "../../data-core/url/IUrl";
 import { SessionUser } from "../../common/tool/session-user";
-import { Router } from '@angular/router';  
-import { Base64 } from '../../common/tool/base64';
+import { Router } from "@angular/router";
+import { Base64 } from "../../common/tool/base64";
 @Injectable()
 export class UserLoginService {
-    sessionUser: SessionUser;
-    form: FormGroup;
-    msg: MessageBar;
-    formVal: { name: string, pwd: string };
-    autoLogin_ = false;
-    jpwd_ = false;
-    constructor(private httpService: HowellAuthHttpService
-        , private router: Router) {
-        this.sessionUser = new SessionUser();
-        this.form = new FormGroup({
-            name: new FormControl(''),
-            pwd: new FormControl('')
+  sessionUser: SessionUser;
+  form: FormGroup;
+  msg: MessageBar;
+  formVal: { name: string; pwd: string };
+  autoLogin_ = false;
+  jpwd_ = false;
+  constructor(
+    private httpService: HowellAuthHttpService,
+    private router: Router
+  ) {
+    this.sessionUser = new SessionUser();
+    this.form = new FormGroup({
+      name: new FormControl(""),
+      pwd: new FormControl(""),
+    });
+    this.msg = new MessageBar();
+  }
+
+  login() {
+    const formVal: { name: string; pwd: string } = this.form.value;
+    if (!formVal.name) this.msg.response_warning("请输入账号");
+    else if (!formVal.pwd) this.msg.response_warning("请输入密码");
+    else {
+      this.formVal = formVal;
+      return this.auth(formVal.name);
+    }
+  }
+
+  urlAuthLogin(param: { Auto: string }) {
+    if (param && param.Auto) {
+      let base64 = new Base64(),
+        urlParam = base64.decode(param.Auto),
+        paramSplit = urlParam.split("&");
+      try {
+        this.form.patchValue({
+          name: paramSplit[0],
+          pwd: paramSplit[1],
         });
-        this.msg = new MessageBar();
+        this.login();
+      } catch {}
     }
+  }
 
-    login() {
-        const formVal: { name: string, pwd: string } = this.form.value;
-        if (!formVal.name)
-            this.msg.response_warning('请输入账号');
-        else if (!formVal.pwd)
-            this.msg.response_warning('请输入密码');
-        else {
-            this.formVal = formVal;
-            this.auth(formVal.name);
-        }
+  fillUserForm() {
+    if (this.sessionUser.autoLogin) {
+      this.form.patchValue({
+        name: this.sessionUser.name,
+        pwd: this.sessionUser.pwd,
+      });
+      this.jpwd_ = true;
+      this.autoLogin_ = true;
+      this.login();
+    } else if (this.sessionUser.memoryPwd) {
+      this.form.patchValue({
+        name: this.sessionUser.name,
+        pwd: this.sessionUser.pwd,
+      });
+      this.jpwd_ = true;
     }
+  }
 
-    urlAuthLogin(param:{ Auto:string}){
-        if(param&&param.Auto){
-            let base64=new Base64(),urlParam = base64.decode(param.Auto)
-            ,paramSplit = urlParam.split('&');
-           try{
-            this.form.patchValue({
-                name:paramSplit[0],
-                pwd: paramSplit[1]
-            });
-            this.login();
-           }catch{
-               
-           }
-        } 
-    }
+  handleLoginError2<T>(operation = "operation", result?: T) {
+    return (error: any): Observable<T> => {
+      if (error.status == 403) {
+        this.msg.response_Error("账号或密码错误");
+      }
+      return of(result as T);
+    };
+  }
 
-    fillUserForm() {
-        if (this.sessionUser.autoLogin) {
-            this.form.patchValue({
-                name: this.sessionUser.name,
-                pwd: this.sessionUser.pwd
-            });
-            this.jpwd_ = true;
-            this.autoLogin_ = true;
-            this.login();
-        }
-        else if (this.sessionUser.memoryPwd) {
-            this.form.patchValue({
-                name: this.sessionUser.name,
-                pwd: this.sessionUser.pwd
-            });
-            this.jpwd_ = true;
-        }
-    }
+  handleLoginError<T>(operation = "operation", result?: T) {
+    return (error: any): Observable<T> => {
+      if (error.status == 403) {
+        let header = error.headers as Headers,
+          userUrl = new User().login(this.formVal.name);
+        let digest = new Digest(header, new BaseUrl().user);
+        var challenge = digest.parseServerChallenge(null);
+        let authHeader = digest.generateRequestHeader(
+          1,
+          challenge,
+          this.formVal.name,
+          this.formVal.pwd,
+          "GET",
+          userUrl
+        );
+        this.httpService
+          .auth(userUrl, authHeader)
+          .pipe(catchError(this.handleLoginError2<any>()))
+          .subscribe(
+            (result: {
+              Id: string;
+              Role: {
+                PictureData: number;
+                PrivacyData: number;
+                StaticData: number;
+                UserData: number;
+              }[];
+              Resources: Array<{
+                Id: string;
+                Name: string;
+                ResourceType: number;
+              }>;
+            }) => {
+              if (result) {
+                console.log(result);
+                // sessionStorage.setItem('userid', );
+                if (
+                  result.Role &&
+                  result.Role.length > 0 &&
+                  result.Role[0].PictureData === 1 &&
+                  result.Role[0].PrivacyData === 1 &&
+                  result.Role[0].StaticData === 1 &&
+                  result.Role[0].UserData === 1
+                ) {
+                  this.router.navigateByUrl("system-mode");
+                } else {
+                  this.router.navigateByUrl("waste-regulation");
+                }
 
-    handleLoginError2<T>(operation = 'operation', result?: T) {
-        return (error: any): Observable<T> => {
-            if (error.status == 403) {
-                this.msg.response_Error('账号或密码错误');
+                this.memory(
+                  this.formVal.name,
+                  this.formVal.pwd,
+                  result.Id,
+                  result.Resources
+                );
+              }
             }
-            return of(result as T);
-        };
-    }
+          );
+      }
+      return of(result as T);
+    };
+  }
 
-    handleLoginError<T>(operation = 'operation', result?: T) {
-        return (error: any): Observable<T> => {
-            if (error.status == 403) {
-                let header = error.headers as Headers, userUrl = new User().login(this.formVal.name);
-                let digest = new Digest(header, new BaseUrl().user);
-                var challenge = digest.parseServerChallenge(null);
-                let authHeader = digest.generateRequestHeader(1, challenge, this.formVal.name, this.formVal.pwd, 'GET', userUrl);
-                this.httpService.auth(userUrl, authHeader).pipe(
-                    catchError(this.handleLoginError2<any>())
-                )
-                    .subscribe((result: {
-                        Id: string,
-                        Role: {
-                            PictureData: number
-                            PrivacyData: number
-                            StaticData: number
-                            UserData: number
-                        }[],
-                        Resources:Array<{Id:string,Name:string,ResourceType:number}>
-                    }) => {
-                        if (result) { 
-                            console.log(result);
-                            // sessionStorage.setItem('userid', );
-                            if (result.Role&&result.Role.length>0
-                                &&result.Role[0].PictureData === 1
-                                && result.Role[0].PrivacyData === 1
-                                && result.Role[0].StaticData === 1
-                                && result.Role[0].UserData === 1) {
-                                this.router.navigateByUrl('system-mode');
-                            } else { this.router.navigateByUrl('waste-regulation'); }
+  memory(
+    name: string,
+    pwd: string,
+    id: string,
+    userDivision: Array<{ Id: string; Name: string; ResourceType: number }>
+  ) {
+    this.sessionUser.autoLogin = this.autoLogin_;
+    this.sessionUser.memoryPwd = this.jpwd_;
+    this.sessionUser.name = name;
+    this.sessionUser.pwd = pwd;
+    this.sessionUser.id = id;
+    this.sessionUser.userDivision = userDivision;
+  }
 
-                            this.memory(this.formVal.name, this.formVal.pwd, result.Id,result.Resources);
-                        }
-                    });
-            }
-            return of(result as T);
-        };
-    }
-
-    memory(name: string, pwd: string, id: string,userDivision:Array<{Id:string,Name:string,ResourceType:number}>) {
-        this.sessionUser.autoLogin = this.autoLogin_;
-        this.sessionUser.memoryPwd = this.jpwd_;
-        this.sessionUser.name = name;
-        this.sessionUser.pwd = pwd;
-        this.sessionUser.id = id;
-        this.sessionUser.userDivision=userDivision;
-
-    }
-
-    async auth(name: string) {
-        const promise = await this.httpService.auth(new User().login(name),
-            new HttpHeaders({ 'X-WebBrowser-Authentication': 'Forbidden' })
-        ).pipe(
-            catchError(this.handleLoginError<any>())
-        ).toPromise();
-    }
+  async auth(name: string) {
+    return this.httpService
+      .auth(
+        new User().login(name),
+        new HttpHeaders({ "X-WebBrowser-Authentication": "Forbidden" })
+      )
+      .pipe(catchError(this.handleLoginError<any>()))
+      .toPromise();
+  }
 }
-
-
