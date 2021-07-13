2cf28df7e028b6c7f3e6e796ff20eec7d2a4c8d3
panminxiang <1061886912@qq.com>
2021-07-13 16:50:37 +0800

    time.service.ts 添加了些注释

--- SmartDataApp/src/app/common/tool/time.service.ts
+++ SmartDataApp/src/app/common/tool/time.service.ts
@@ -2,28 +2,39 @@ import { Injectable } from "@angular/core";
 
 @Injectable()
 export class TimeToolService {
-    currentTime: { YYYY: number, MM: number, dd: number, HH: number, mm: string, ss: string, week: string };
-    getTimeForMat(myDate: Date) {
-        let weekArray = new Array('天', '一', '二', '三', '四', '五', '六');
-        let week = weekArray[myDate.getDay()];
-        const fillZero = (val: number) => {
-            return val < 10 ? '0' + val : val + '';
-        }
-        return {
-            YYYY: myDate.getFullYear(),
-            MM: myDate.getMonth() + 1,
-            dd: myDate.getDate(),
-            HH: myDate.getHours(),
-            mm: fillZero(myDate.getMinutes()),
-            ss: fillZero(myDate.getSeconds()),
-            week: '星期' + week
-        }
-    }
+  currentTime: {
+    YYYY: number;
+    MM: number;
+    dd: number;
+    HH: number;
+    mm: string;
+    ss: string;
+    week: string;
+  };
+  getTimeForMat(myDate: Date) {
+    // 0代表星期日
+    let weekArray = new Array("天", "一", "二", "三", "四", "五", "六");
+    let week = weekArray[myDate.getDay()];
+    const fillZero = (val: number) => {
+      return val < 10 ? "0" + val : val + "";
+    };
+    return {
+      YYYY: myDate.getFullYear(),
+      MM: myDate.getMonth() + 1, //0表示一月
+      dd: myDate.getDate(),
+      HH: myDate.getHours(),
+      mm: fillZero(myDate.getMinutes()),
+      //   mm: myDate.getMinutes().toString().padStart(2, "0"),
+      ss: fillZero(myDate.getSeconds()),
+      //   ss: myDate.getSeconds().toString().padStart(2, "0"),
+      week: "星期" + week,
+    };
+  }
 
-    runTime() {  this.currentTime = this.getTimeForMat(new Date()); 
-        setInterval(() => {
-            this.currentTime = this.getTimeForMat(new Date()); 
-
-        }, 1000);
-    }
+  runTime() {
+    this.currentTime = this.getTimeForMat(new Date());
+    setInterval(() => {
+      this.currentTime = this.getTimeForMat(new Date());
+    }, 1000);
+  }
 }
