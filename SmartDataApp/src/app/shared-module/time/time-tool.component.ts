bbcac9ee76653c02a3d4eaf69a82e421a8c60297
panminxiang <1061886912@qq.com>
2021-08-31 10:15:24 +0800

    修改 time-tool

--- SmartDataApp/src/app/shared-module/time/time-tool.component.ts
+++ SmartDataApp/src/app/shared-module/time/time-tool.component.ts
@@ -1,21 +1,15 @@
-import { Component, OnInit } from '@angular/core'; 
-import { TimeToolService } from '../../common/tool/time.service';
+import { Component, OnInit } from "@angular/core";
+import { TimeToolService } from "../../common/tool/time.service";
 @Component({
-  selector: 'app-time-tool',
-  templateUrl: './time-tool.component.html',
-  styleUrls: ['./time-tool.component.styl'],
-  providers:[TimeToolService]
+  selector: "app-time-tool",
+  templateUrl: "./time-tool.component.html",
+  styleUrls: ["./time-tool.component.css"],
+  providers: [TimeToolService],
 })
 export class TimeToolComponent implements OnInit {
-  
   constructor(private service: TimeToolService) {
-    this.service.runTime(); 
-    
-   }
-
-  ngOnInit() {
-  
+    this.service.runTime();
   }
 
+  ngOnInit() {}
 }
-
